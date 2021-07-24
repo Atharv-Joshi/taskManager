937ed66dfbeb62a973147d5fd017fec2888852d8
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-07-24 12:28:09 +0530

    used async await instead of then

--- src/index.js
+++ src/index.js
@@ -17,100 +17,121 @@ const port = process.env.PORT || 3000
 //if not written then incoming object is indentified as undefined.
 app.use(express.json())
 
-
+//-------------------------------------User endpoints-----------------------------------------------------------------------------------------------
 //post user
-app.post('/users' , (req , res) => {
+app.post('/users' , async (req , res) => {
     const user = new User(req.body)
-    user.save().then(
-        (user) =>{
-            res.send(user)
-        }
-    ).catch(
-        (e) =>{
-            res.status(400).send(e)
-        }
-    )
+    try{
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
 //read all users
-app.get('/users' , (req , res) => {
-    User.find({}).then(
-        (users) => {
-            res.send(users)
-        }
-    ).catch(
-        (e) =>{
-            res.status(500).send()
-        }
-    )
+app.get('/users' , async (req , res) => {
+    try{
+        const users = await User.find({})
+        res.status(201).send(users)
+    }catch(e){
+        res.status(500).send(e)
+    }
 })
 
 //read user by id
 //:id means that it is a placeholder for dynamic values (here user id will be placed) which can be accessed by req.params.id
-app.get('/users/:id' , (req , res) =>{
+app.get('/users/:id' , async (req , res) =>{
     const _id = req.params.id
-    User.findById(_id).then(
-        (user) =>{
-            if(!user){
-                console.log(user)
-                return res.status(404).send()
-            }
-            res.send(user)
+    try{
+        const user = await User.findById(_id)
+        if(!user){
+            return res.status(404).send()
         }
-    ).catch(
-        (e) =>{
-            res.status(500).send(e)
-        }
-    )
+        return res.status(201).send(user)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
 }
 )
 
+//-------------------------------------User endpoints-----------------------------------------------------------------------------------------------
+
+
+//-------------------------------------Task endpoints-----------------------------------------------------------------------------------------------
 
 //post task
-app.post('/tasks' , (req , res) => {
+app.post('/tasks' , async (req , res) => {
     const task = Task(req.body)
-    task.save().then(
-        (task) =>{
-            res.send(task)
-        }
-    ).catch(
-        (e) =>{
-            res.status(400).send(e)
-        }
-    )
+    // task.save().then(
+    //     (task) =>{
+    //         res.send(task)
+    //     }
+    // ).catch(
+    //     (e) =>{
+    //         res.status(400).send(e)
+    //     }
+    // )
+    try{
+        await task.save()
+        res.status(201).send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
 //read all tasks
-app.get('/tasks' , (req , res) => {
-    Task.find({}).then(
-        (tasks) =>{
-            res.send(tasks)
-        }
-    ).catch(
-        (e) =>{
-            res.status(500).send()
-        }
-    )
+app.get('/tasks' , async (req , res) => {
+    // Task.find({}).then(
+    //     (tasks) =>{
+    //         res.send(tasks)
+    //     }
+    // ).catch(
+    //     (e) =>{
+    //         res.status(500).send()
+    //     }
+    // )
+    try{
+        const tasks = await Task.find({})
+        res.status(201).send(tasks)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+    
 })
 
 //read task by id
 //:id means that it is a placeholder for dynamic values (here task id will be placed) which can be accessed by req.params.id
-app.get('/tasks/:id' , (req , res) =>{
+app.get('/tasks/:id' , async (req , res) =>{
     const _id = req.params.id
-    Task.findById(_id).then(
-        (task) =>{
-            if(!task){
-                return res.status(404).send()
-            }
-            res.send(task)
+    // Task.findById(_id).then(
+    //     (task) =>{
+    //         if(!task){
+    //             return res.status(404).send()
+    //         }
+    //         res.send(task)
+    //     }
+    // ).catch(
+    //     (e) =>{
+    //         res.status(500).send(e)
+    //     }
+    // )
+    try{
+        const task = await Task.findById(_id)
+        if(!task){
+            return res.send(404).send()
         }
-    ).catch(
-        (e) =>{
-            res.status(500).send(e)
-        }
-    )
+        return res.status(201).send(task)
+    }
+    catch(e){
+        res.status(500).send()
+    }
 })
 
+
+//-------------------------------------Task endpoints-----------------------------------------------------------------------------------------------
 
 
 app.listen(port , () =>{
