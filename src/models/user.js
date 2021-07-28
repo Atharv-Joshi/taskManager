e7d160dee4524f18bbba8ac1c23c57e98a30985d
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-07-28 16:23:20 +0530

    auth tokens on login signuo

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 //User schema
 const userSchema = new mongoose.Schema(
@@ -42,7 +43,15 @@ const userSchema = new mongoose.Schema(
                     throw new Error('Password cannot contain "password"')
                 }
             }
-        }
+        },
+        tokens : [
+            {
+                token : {
+                    type : String,
+                    required : true
+                }
+            }
+        ]
     }
 )
 
@@ -69,6 +78,14 @@ userSchema.statics.findByCredentials = async  (email , password) => {
         throw new Error('Unable to Login')
     }
     return user
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({_id : user._id.toString()} , 'taskmanager')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
 }
 
 //User Model
